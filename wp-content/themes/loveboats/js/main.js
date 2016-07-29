6e34a19ed6169bf1c6a1f711ee0e55936b06117d
mircearek <mircearek@gmail.com>
2016-07-29 16:32:07 +0300

    width for template only

--- wp-content/themes/loveboats/js/main.js
+++ wp-content/themes/loveboats/js/main.js
@@ -1,8 +1,8 @@
 var $ = jQuery.noConflict();
 $(document).ready(function() {
 	
-        
-    $('body').css({
+        //1290
+    $('.print-template-wrapper').css({
         'max-width' : $( window ).width() + 'px',
         'overflow' : 'hidden',
         'display' : 'inline-block'
@@ -291,7 +291,7 @@ $(document).ready(function() {
 
 
 $(window).resize(function(){
-    $('body').css({
+    $('.print-template-wrapper').css({
         'width' : $( window ).width() + 'px',
         'overflow' : 'hidden',
         'display' : 'inline-block'
